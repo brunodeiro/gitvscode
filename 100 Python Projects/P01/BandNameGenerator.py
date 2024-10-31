3cf1ef65498bc042c51ab25c9a61551e5705c399
Bruno Deiro <bruno_deiro@live.com>
2024-10-31 08:48:58 -0300

    Project 01 - band name generator

---
+++ 100 Python Projects/P01/BandNameGenerator.py
@@ -0,0 +1,11 @@
+""""
+Este projeto tem como objetivo a criação de um gerador de nomes de bandas utilizando a cidade em que cresceu e o seu pet| 
+This project has a goal a band name generator using the city you grow up and your pet name
+"""
+
+print("Welcome to the Band Name Generator. \n") #Introduction
+
+city = input("What's the name of the city you grew up in ? \n") #Collecting the element "city"
+pet = input("What's your pet name ? \n") #Collecting the element "pet"
+
+print(f'Your band name could be "{pet} {city}"') #Result
